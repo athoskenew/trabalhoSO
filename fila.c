fab530622adcc4c617d987c16253729e68880ae1
athoskenew <33551922+athoskenew@users.noreply.github.com>
2019-10-30 12:39:57 -0300

    Testes de funções extra
    Adicionado funções extra para testar struct de processos.

--- fila.c
+++ fila.c
@@ -1,20 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct Processo{
+	int id;
+	int tipo;
+};
+typedef struct Processo Processo
+
 struct Fila {
 
 	int capacidade;
-	float *dados;
+	Processo *dados;
 	int primeiro;
 	int ultimo;
 	int nItens; 
 
 };
-
+	
 void criarFila( struct Fila *f, int c ) { 
 
 	f->capacidade = c;
 	f->dados = (float*) malloc (f->capacidade * sizeof(float));
+	f->primeiro = 0;
+	f->ultimo = -1;
+	f->nItens = 0; 
+
+}
+
+void criarFilaProcesso( struct Fila *f,struct Processo *p, int c ) { 
+
+	f->capacidade = c;
+	f->dados = (Processo*) malloc (f->capacidade * sizeof(Processo));
 	f->primeiro = 0;
 	f->ultimo = -1;
 	f->nItens = 0; 
@@ -64,6 +80,22 @@ void mostrarFila(struct Fila *f){
 		printf("%.2f\t",f->dados[i++]);
 
 		if (i == f->capacidade)
+			i=0;
+
+	}
+	printf("\n\n");
+
+}
+
+void mostrarFilaProcesso(struct Processo *p){
+
+	int cont, i;
+
+	for ( cont=0, i= p->primeiro; cont < p->nItens; cont++){
+
+		printf("%d\t",p->id);
+
+		if (i == p->capacidade)
 			i=0;
 
 	}
